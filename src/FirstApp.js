f39f69dd40dd066568262f3c52a2b55278cb9d37
David Toledo <dtoledoz0207@gmail.com>
2020-10-20 10:45:17 -0500

    use-fragment-tag-to-return-elements-in-FirstAppComponent

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,7 +1,19 @@
 import React from 'react';
+//import React, { Fragment } from 'react';
 
 const FirstApp = () => {
-  return <h1>Hello world</h1>;
+  return (
+    // <Fragment>
+    //   <h1>Hello world</h1>
+    //   <p>My first App</p>
+    // </Fragment>
+
+    // This is also a fragment
+    <>
+      <h1>Hello world</h1>
+      <p>My first App</p>
+    </>
+  );
 }
 
 export default FirstApp;
