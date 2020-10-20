18bd8831bc1172d5f789bbd90f9919b34824c168
David Toledo <dtoledoz0207@gmail.com>
2020-10-20 11:45:50 -0500

    use-defaultProps-to-set-default-values-to-properties

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-const FirstApp = ({greeting}) => {
+const FirstApp = ({greeting, subtitle}) => {
 
   return (
     // <Fragment>
@@ -13,13 +13,17 @@ const FirstApp = ({greeting}) => {
     // This is also a fragment
     <>
       <h1>{greeting}</h1>
-      <p>My first App</p>
+      <p>{subtitle}</p>
     </>
   );
 }
 
 FirstApp.propTypes = {
   greeting: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+  subtitle: "I'm a subtitle"
 }
 
 export default FirstApp;
