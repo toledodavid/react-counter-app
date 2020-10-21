aead4da29535d76f087931f1003a14a28da66d36
David Toledo <dtoledoz0207@gmail.com>
2020-10-21 10:36:49 -0500

    add-event-onClick-in-CounterAppComponent

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types';
 
 
 const CounterApp = ({value}) => {
+
+  const handleAdd = (event) => {
+    console.log(event);
+  }
+
   return(
     <>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 }
