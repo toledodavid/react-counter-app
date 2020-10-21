7af632a4817ff013c9f2b039cd515b54c4790424
David Toledo <dtoledoz0207@gmail.com>
2020-10-21 11:32:41 -0500

    add-useState-hook-in-CounterAppComponent

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 
 const CounterApp = ({value}) => {
 
-  const handleAdd = (event) => {
-    console.log(event);
+  const [counter, setCounter] = useState(0);
+
+  const handleAdd = () => {
+    setCounter(counter + 1);
+    //setCounter((c) => c + 1);
   }
 
   return(
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
 
       <button onClick={handleAdd}>+1</button>
     </>
