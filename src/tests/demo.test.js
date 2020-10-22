7482dbd5a39d934f8be5504c93b4d7d17d261749
David Toledo <dtoledoz0207@gmail.com>
2020-10-22 13:06:32 -0500

    modify-demo-test-to-use-jest-methods-describe-expect-toBe

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,9 +1,15 @@
 
+describe('Tests in demo.test.js file', () => {
+  test('the two strings must be equals', () => {
 
-test('Must be true', () => {
-  const isActive = false;
-
-  if (!isActive) {
-    throw new Error('It is not Active');
-  }
+    // 1. Inicializacion
+    const message1 = 'Hello world';
+  
+    // 2. Estimulo
+    const message2 = `Hello world`;
+  
+    // Observar el comportamiento
+    expect(message1).toBe(message2);
+  
+  });
 });
