03c84770b1f342c6774926179bb1eb313de7ee68
David Toledo <dtoledoz0207@gmail.com>
2020-10-27 11:35:01 -0600

    install-enzyme-testing-and-create-a-new-test-for-FirstApp-component

--- src/tests/FirstApp.test.js
+++ src/tests/FirstApp.test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import FirstApp from '../FirstApp';
 
 describe('Tests in < FirstApp /> Component', () => {
-  test("It should show a message - Hello, I'm Toledo", () => {
+  // test("It should show a message - Hello, I'm Toledo", () => {
+  //   const greeting = "Hello, I'm Toledo";
+  //   const { getByText } = render(<FirstApp greeting={greeting} />);
+  //   expect(getByText(greeting)).toBeInTheDocument();
+  // });
 
+  test('It should show <FirstApp /> component correctly', () => {
     const greeting = "Hello, I'm Toledo";
-    const { getByText } = render(<FirstApp greeting={greeting} />);
+    const wrapper = shallow( <FirstApp greeting={greeting} /> );
 
-    expect(getByText(greeting)).toBeInTheDocument();
-
+    expect(wrapper).toMatchSnapshot();
   });
 });
